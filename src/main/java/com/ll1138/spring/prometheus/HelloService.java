104f62f3cd5e1a827a7d5ccb1e1d5bb3cefb89da
Leszek Leszczynski <L.Leszczynski@gmail.com>
2019-07-22 19:44:29 +0200

    Added dummy load operation

--- src/main/java/com/ll1138/spring/prometheus/HelloService.java
+++ src/main/java/com/ll1138/spring/prometheus/HelloService.java
@@ -3,6 +3,8 @@ package com.ll1138.spring.prometheus;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.Random;
+
 @RestController
 @RequestMapping("/api/v1/hello")
 public class HelloService {
@@ -10,5 +12,10 @@ public class HelloService {
     @RequestMapping("/world")
     String hello() {
         return "Hello World!";
+    }
+
+    @RequestMapping("/sort")
+    int[] sort() {
+        return new Random().ints(10000).sorted().toArray();
     }
 }
